709a62d304b76ea0ecf1c428d9ee04630a70ba72
Billy Arante <arantebillywilson@gmail.com>
2020-11-14 14:35:43 +0800

    add: Dynamic grid size

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 const engine = Engine.create();
 const { world } = engine;
-const width = 800;
+const width = 600;
 const height = 600;
 const render = Render.create({
   element: document.body,
@@ -15,24 +15,11 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-  mouse: Mouse.create(render.canvas)
-}));
-
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true })
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }), // top
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }), // right
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }), // bottom
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }) // left
 ];
 World.add(world, walls);
-
-// Random Shapes
-for (let i = 0; i < 30; i++) {
-  if (Math.random() > 0.5) {
-    World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-  } else {
-    World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35));
-  }
-}
