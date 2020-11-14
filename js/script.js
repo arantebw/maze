c87704c237862cb9fab6a0aac4528f1f31b9dc2b
Billy Arante <arantebillywilson@gmail.com>
2020-11-14 12:08:30 +0800

    add: Random shapes, colors, and fills

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 const engine = Engine.create();
 const { world } = engine;
+const width = 800;
+const height = 600;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: false,
+    width,
+    height
   }
 });
 Render.run(render);
@@ -25,4 +28,11 @@ const walls = [
 ];
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random Shapes
+for (let i = 0; i < 30; i++) {
+  if (Math.random() > 0.5) {
+    World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
+  } else {
+    World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35));
+  }
+}
